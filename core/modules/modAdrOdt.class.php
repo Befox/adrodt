7a084ebe82b2d110785f4c7aef6efe08106b3058
Jérôme Andanson <jandanson@befox.fr>
2015-08-12 11:14:30 +0200

    Sur la branche master: syntax error #*************************************************************** # Not in a bugXXX branch ! #***************************************************************

--- core/modules/modAdrOdt.class.php
+++ core/modules/modAdrOdt.class.php
@@ -87,7 +87,7 @@ class modAdrOdt extends DolibarrModules
 		//							'dir' => array('output' => 'othermodulename'),      // To force the default directories names
 		//							'workflow' => array('WORKFLOW_MODULE1_YOURACTIONTYPE_MODULE2'=>array('enabled'=>'! empty($conf->module1->enabled) && ! empty($conf->module2->enabled)', 'picto'=>'yourpicto@mymodule')) // Set here all workflow context managed by module
 		//                        );
-		$this->module_parts = array('substitutions' => 1)
+		$this->module_parts = array('substitutions' => 1);
 
 
 		// Data directories to create when module is enabled.
