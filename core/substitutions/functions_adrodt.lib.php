a4bf4a729ab31c7514ab1119b3d8d36f8c398203
Jérôme Andanson <jandanson@befox.fr>
2015-08-12 14:24:41 +0200

    Sur la branche master: .. #*************************************************************** # Not in a bugXXX branch ! #***************************************************************

--- core/substitutions/functions_adrodt.lib.php
+++ core/substitutions/functions_adrodt.lib.php
@@ -12,26 +12,38 @@ function adrodt_completesubstitutionarray(&$substitutionarray,$langs,$object)
 {
    global $conf,$db;
  
-   $myvalue='Put here calculated value to insert';
-   $substitutionarray['adrodt_deliv_name']=$myvalue;
-   $substitutionarray['adrodt_deliv_firstname']=$myvalue;
-   
-   
-   /*
-    * 
-    * */
-    $substitutionarray['myowntag'] = '';
-    
-	$usecontact=false;
-	$arrayidcontact=$object->getIdContact('external','CUSTOMER');
-	if (count($arrayidcontact) > 0)
+   	$arrayidcontact = $object->getIdContact('external','CUSTOMER');
+	if (count($arrayidcontact) > 0 && $object->fetch_contact($arrayidcontact[0]) == true)
 	{
-		$substitutionarray['myowntag'].=print_r($arrayidcontact, true);
-		$usecontact=true;
-		$result=$object->fetch_contact($arrayidcontact[0]);
+		$substitutionarray['adrodt_deliv_name']		 	= $object->name;
+		$substitutionarray['adrodt_deliv_lastname'] 	= $object->contact->lastname;
+		$substitutionarray['adrodt_deliv_firstname'] 	= $object->contact->firstname;
+		$substitutionarray['adrodt_deliv_address'] 		= $object->contact->address;
+		$substitutionarray['adrodt_deliv_zip'] 			= $object->contact->zip;
+		$substitutionarray['adrodt_deliv_town'] 		= $object->contact->town;
+		$substitutionarray['adrodt_deliv_department'] 	= $object->contact->department;
+		$substitutionarray['adrodt_deliv_state'] 		= $object->contact->state;
+		$substitutionarray['adrodt_deliv_country'] 		= $object->contact->country;
+		$substitutionarray['adrodt_deliv_email'] 		= $object->contact->email;
+		$substitutionarray['adrodt_deliv_phone'] 		= $object->contact->phone_pro;
+		$substitutionarray['adrodt_deliv_fax'] 			= $object->contact->fax;
+	}
+	else
+	{
+		$substitutionarray['adrodt_deliv_lastname'] 	= $object->lastname;
+		$substitutionarray['adrodt_deliv_firstname'] 	= $object->firstname;
+		$substitutionarray['adrodt_deliv_address'] 		= $object->address;
+		$substitutionarray['adrodt_deliv_zip'] 			= $object->zip;
+		$substitutionarray['adrodt_deliv_town'] 		= $object->town;
+		$substitutionarray['adrodt_deliv_department'] 	= $object->department;
+		$substitutionarray['adrodt_deliv_state'] 		= $object->state;
+		$substitutionarray['adrodt_deliv_country'] 		= $object->country;
+		$substitutionarray['adrodt_deliv_email'] 		= $object->email;
+		$substitutionarray['adrodt_deliv_phone'] 		= $object->phone;
+		$substitutionarray['adrodt_deliv_fax'] 			= $object->fax;
 	}
 	
-	$substitutionarray['myowntag'].='--------------------------------------------------------------------'.print_r($object->contact, true);
-	$substitutionarray['myowntag'].='--------------------------------------------------------------------'.print_r($object, true);
+	$substitutionarray['myowntag'].=''.print_r($substitutionarray, true);
+	$substitutionarray['myowntag'].='-------------------'.print_r($object, true);
    
 }
